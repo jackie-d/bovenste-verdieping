dd47083069e0c460b32659dd1038b8788180bdff
Jackie Degl'Innocenti <info@jackied.it>
2021-01-20 16:18:43 +0100

    Improved the page views and content

---
+++ Pages/Tuin.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BovensteVerdieping.Pages
+{
+    public class TuinModel : PageModel
+    {
+        private readonly ILogger<TuinModel> _logger;
+
+        public TuinModel(ILogger<TuinModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
